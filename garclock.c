592d595d3cc266c4f72325bfa6555a82f62bf0a8
ccgargantua <lovelygargantua@gmail.com>
2024-09-07 14:05:32 -0400

    linux final

--- garclock.c
+++ garclock.c
@@ -1,172 +1,174 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 #ifdef _WIN32
     #include <windows.h>
 #elif defined(__unix__)
     #include <sys/time.h>
+    #include <signal.h>
 #else
     #warning "Only written to support windows and unix-like systems."
 #endif
 
 
 
-const char ascii_zero[10][11]  = { " 00000000 ", \
-                                   "0000000000", \
-                                   "000    000", \
-                                   "00      00", \
-                                   "00      00", \
-                                   "00      00", \
-                                   "00      00", \
-                                   "000    000", \
-                                   "0000000000", \
-                                   " 00000000 " };
+const char ascii_zero[10][14]  = { "  00000000  ", \
+                                   " 0000000000 ", \
+                                   " 000    000 ", \
+                                   " 00      00 ", \
+                                   " 00      00 ", \
+                                   " 00      00 ", \
+                                   " 00      00 ", \
+                                   " 000    000 ", \
+                                   " 0000000000 ", \
+                                   "  00000000  " };
 
-const char ascii_one[10][11]   = { "   1111   ", \
-                                   " 111111   ", \
-                                   "   1111   ", \
-                                   "   1111   ", \
-                                   "   1111   ", \
-                                   "   1111   ", \
-                                   "   1111   ", \
-                                   "   1111   ", \
-                                   " 11111111 ", \
-                                   "1111111111" };
+const char ascii_one[10][14]   = { "    1111    ", \
+                                   "  111111    ", \
+                                   "    1111    ", \
+                                   "    1111    ", \
+                                   "    1111    ", \
+                                   "    1111    ", \
+                                   "    1111    ", \
+                                   "    1111    ", \
+                                   "  11111111  ", \
+                                   " 1111111111 " };
 
-const char ascii_two[10][11]   = { "  22222   ", \
-                                   "  222222  ", \
-                                   "222    222", \
-                                   "222    222", \
-                                   "      2222", \
-                                   "    2222  ", \
-                                   "   2222   ", \
-                                   "  2222    ", \
-                                   "2222222222", \
-                                   "2222222222" };
+const char ascii_two[10][14]   = { "   22222    ", \
+                                   "   222222   ", \
+                                   " 222    222 ", \
+                                   " 222    222 ", \
+                                   "       2222 ", \
+                                   "     2222   ", \
+                                   "    2222    ", \
+                                   "   2222     ", \
+                                   " 2222222222 ", \
+                                   " 2222222222 " };
 
-const char ascii_three[10][11] = { " 333333333", \
-                                   "3333333333", \
-                                   "        33", \
-                                   "       333", \
-                                   " 333333333", \
-                                   "3333333333", \
-                                   "        33", \
-                                   "       333", \
-                                   " 333333333", \
-                                   "3333333333" };
+const char ascii_three[10][14] = { "  333333333 ", \
+                                   " 3333333333 ", \
+                                   "         33 ", \
+                                   "        333 ", \
+                                   "  333333333 ", \
+                                   " 3333333333 ", \
+                                   "         33 ", \
+                                   "        333 ", \
+                                   "  333333333 ", \
+                                   " 3333333333 " };
 
-const char ascii_four[10][11]  = { "    444444", \
-                                   "   4444444", \
-                                   "  444  444", \
-                                   " 444   444", \
-                                   "444    444", \
-                                   "4444444444", \
-                                   "4444444444", \
-                                   "       444", \
-                                   "       444", \
-                                   "       444" };
+const char ascii_four[10][14]  = { "     444444 ", \
+                                   "    4444444 ", \
+                                   "   444  444 ", \
+                                   "  444   444 ", \
+                                   " 444    444 ", \
+                                   " 4444444444 ", \
+                                   " 4444444444 ", \
+                                   "        444 ", \
+                                   "        444 ", \
+                                   "        444 " };
 
-const char ascii_five[10][11]  = { "5555555555", \
-                                   "5555555555", \
-                                   "555       ", \
-                                   "555       ", \
-                                   " 55555555 ", \
-                                   "   5555555", \
-                                   "       555", \
-                                   "555    555", \
-                                   "5555555555", \
-                                   " 55555555 " };
+const char ascii_five[10][14]  = { " 5555555555 ", \
+                                   " 5555555555 ", \
+                                   " 555        ", \
+                                   " 555        ", \
+                                   "  55555555  ", \
+                                   "    5555555 ", \
+                                   "        555 ", \
+                                   " 555    555 ", \
+                                   " 5555555555 ", \
+                                   "  55555555  " };
 
-const char ascii_six[10][11]   = { " 66666666 ", \
-                                   "6666666666", \
-                                   "666    666", \
-                                   "666       ", \
-                                   "666666666 ", \
-                                   "6666666666", \
-                                   "66      66", \
-                                   "66      66", \
-                                   "6666666666", \
-                                   " 66666666 " };
+const char ascii_six[10][14]   = { "  66666666  ", \
+                                   " 6666666666 ", \
+                                   " 666    666 ", \
+                                   " 666        ", \
+                                   " 666666666  ", \
+                                   " 6666666666 ", \
+                                   " 66      66 ", \
+                                   " 66      66 ", \
+                                   " 6666666666 ", \
+                                   "  66666666  " };
 
-const char ascii_seven[10][11] = { "777777777 ", \
-                                   "7777777777", \
-                                   "       777", \
-                                   "       777", \
-                                   "       777", \
-                                   "      777 ", \
-                                   "     777  ", \
-                                   "     777  ", \
-                                   "     777  ", \
-                                   "     777  " };
+const char ascii_seven[10][14] = { " 777777777  ", \
+                                   " 7777777777 ", \
+                                   "        777 ", \
+                                   "        777 ", \
+                                   "        777 ", \
+                                   "       777  ", \
+                                   "      777   ", \
+                                   "      777   ", \
+                                   "      777   ", \
+                                   "      777   " };
 
-const char ascii_eight[10][11] = { " 88888888 ", \
-                                   "8888888888", \
-                                   "88      88", \
-                                   "88      88", \
-                                   " 88888888 ", \
-                                   " 88888888 ", \
-                                   "88      88", \
-                                   "88      88", \
-                                   "8888888888", \
-                                   " 88888888 " };
+const char ascii_eight[10][14] = { "  88888888  ", \
+                                   " 8888888888 ", \
+                                   " 88      88 ", \
+                                   " 88      88 ", \
+                                   "  88888888  ", \
+                                   "  88888888  ", \
+                                   " 88      88 ", \
+                                   " 88      88 ", \
+                                   " 8888888888 ", \
+                                   "  88888888  " };
 
-const char ascii_nine[10][11]  = { " 99999999 ", \
-                                   "9999999999", \
-                                   "99      99", \
-                                   "99      99", \
-                                   "9999999999", \
-                                   " 999999999", \
-                                   "       999", \
-                                   "999    999", \
-                                   "9999999999", \
-                                   " 99999999 " };
+const char ascii_nine[10][14]  = { "  99999999  ", \
+                                   " 9999999999 ", \
+                                   " 99      99 ", \
+                                   " 99      99 ", \
+                                   " 9999999999 ", \
+                                   "  999999999 ", \
+                                   "        999 ", \
+                                   " 999    999 ", \
+                                   " 9999999999 ", \
+                                   "  99999999  " };
 
 
-const char ascii_colon[10][11]   = { "   ::::   ", \
-                                     "   ::::   ", \
-                                     "   ::::   ", \
-                                     "   ::::   ", \
-                                     "          ", \
-                                     "          ", \
-                                     "   ::::   ", \
-                                     "   ::::   ", \
-                                     "   ::::   ", \
-                                     "   ::::   " };
+const char ascii_colon[10][14]   = { "   ::::::   ", \
+                                     "   ::::::   ", \
+                                     "   ::::::   ", \
+                                     "   ::::::   ", \
+                                     "            ", \
+                                     "            ", \
+                                     "   ::::::   ", \
+                                     "   ::::::   ", \
+                                     "   ::::::   ", \
+                                     "   ::::::   " };
 
-const char ascii_slash[10][11]   = { "        //", \
-                                     "       ///", \
-                                     "      /// ", \
-                                     "     ///  ", \
-                                     "    ///   ", \
-                                     "   ///    ", \
-                                     "  ///     ", \
-                                     " ///      ", \
-                                     "///       ", \
-                                     "//        " };
+const char ascii_slash[10][14]   = { "         ///", \
+                                     "        /// ", \
+                                     "       ///  ", \
+                                     "      ///   ", \
+                                     "     ///    ", \
+                                     "    ///     ", \
+                                     "   ///      ", \
+                                     "  ///       ", \
+                                     " ///        ", \
+                                     "///         " };
 
-const char ascii_period[10][11]  = { "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "   ....   ", \
-                                     "   ....   ", \
-                                     "   ....   " };
+const char ascii_period[10][14]  = { "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "    ....    ", \
+                                     "    ....    ", \
+                                     "    ....    " };
 
-const char ascii_space[10][11]   = { "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          ", \
-                                     "          " };
+const char ascii_space[10][14]   = { "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            ", \
+                                     "            " };
 
 
 
@@ -174,6 +176,7 @@ void reset_cursor(void);
 void get_time_string(char *buffer, size_t buffer_size);
 void get_date_string(char *buffer, size_t buffer_size);
 void print_ascii_string(char *src_buffer,  size_t src_buffer_size);
+void handle_signal(int sig);
 
 
 
@@ -193,22 +196,41 @@ int main(void)
 
         if (!SetConsoleMode(hOut, dwMode)) return 1;
 
+
+        signal(SIGINT, handle_signal);
+
+    #else
+
+        struct sigaction sa;
+
+        sa.sa_handler = handle_signal;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+
+        // Register the signal handler using sigaction
+        sigaction(SIGINT, &sa, NULL);
+
     #endif
 
 
-    setvbuf(stdout, NULL, _IOFBF, 1024*10); // disable auto-flush on newline
+    setvbuf(stdout, NULL, _IOFBF, 1024); // disable auto-flush on newline
+
+
+    printf("\033[?25l"); // hide cursor
 
 
     while (1)
     {
         reset_cursor();
+
         char time_buffer[13] = {'\0'};
-        get_time_string(time_buffer, 12);
-        print_ascii_string(time_buffer, 11);
+        get_time_string(time_buffer, 13);
+        print_ascii_string(time_buffer, 12);
 
         char date_buffer[11] = {'\0'};
         get_date_string(date_buffer, 12);
         print_ascii_string(date_buffer, 10);
+
         fflush(stdout);
     }
 
@@ -273,59 +295,45 @@ void print_ascii_string(char *src_buffer,  size_t src_buffer_size)
             {
             case '0':
                 printf("%s", ascii_zero[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_zero[i]);
                 break;
             case '1':
                 printf("%s", ascii_one[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_one[i]);
                 break;
             case '2':
                 printf("%s", ascii_two[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_two[i]);
                 break;
             case '3':
                 printf("%s", ascii_three[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_three[i]);
                 break;
             case '4':
                 printf("%s", ascii_four[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_four[i]);
                 break;
             case '5':
                 printf("%s", ascii_five[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_five[i]);
                 break;
             case '6':
                 printf("%s", ascii_six[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_six[i]);
                 break;
             case '7':
                 printf("%s", ascii_seven[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_seven[i]);
                 break;
             case '8':
                 printf("%s", ascii_eight[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_eight[i]);
                 break;
             case '9':
                 printf("%s", ascii_nine[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_nine[i]);
                 break;
             case ':':
                 printf("%s", ascii_colon[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_colon[i]);
                 break;
             case '/':
                 printf("%s", ascii_slash[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_slash[i]);
                 break;
             case '.':
                 printf("%s", ascii_period[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_period[i]);
                 break;
             case ' ':
                 printf("%s", ascii_space[i]);
-                // dest_buffer += snprintf(dest_buffer, 11, "%s", ascii_space[i]);
                 break;
             default:
                 return;
@@ -334,4 +342,14 @@ void print_ascii_string(char *src_buffer,  size_t src_buffer_size)
         printf("\n");
     }
         printf("\n");
+}
+
+
+
+void handle_signal(int sig)
+{
+    reset_cursor();
+    printf("\033[2J\033[?25h"); //  clear screen, enable cursor
+    fflush(stdout);
+    exit(0);
 }
